899bea9a91c5d949965fd883090a1b233ebc8d8c
Bagus <bagusknusantara@gmail.com>
2025-06-06 17:21:13 +0700

    adding a and d controls to player.py

--- main.py
+++ main.py
@@ -6,22 +6,24 @@ from constants import *
 from player import *
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
-    clock = pygame.time.Clock()
-    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
     player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 0))
+            
+        player.update(dt)
+
+        screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
-        clock.tick(60)
+        
+        dt = clock.tick(60) / 1000.0
     
 
 if __name__ == "__main__":
